93bdc1c7c0fda656f51912e927bee77aad0d72fd
lgfyxx1 <68235325+lgfyxx1@users.noreply.github.com>
2024-06-26 12:18:10 +0800

    Update AutoBuild_Function.sh

--- Scripts/AutoBuild_Function.sh
+++ Scripts/AutoBuild_Function.sh
@@ -2,16 +2,6 @@
 # AutoBuild Module by Hyy2001 <https://github.com/Hyy2001X/AutoBuild-Actions-BETA>
 # AutoBuild Functions
 
-##-----------------Add OpenClash dev core自定义------------------
-echo "Add OpenClash dev core started"
-curl -sL -m 30 --retry 2 https://raw.githubusercontent.com/vernesong/OpenClash/master/core-lateset/dev/clash-linux-arm64.tar.gz -o /tmp/clash.tar.gz
-tar zxvf /tmp/clash.tar.gz -C /tmp >/dev/null 2>&1
-chmod +x /tmp/clash >/dev/null 2>&1
-mkdir -p feeds/luci/applications/luci-app-openclash/root/etc/openclash/core
-mv /tmp/clash feeds/luci/applications/luci-app-openclash/root/etc/openclash/core/clash >/dev/null 2>&1
-rm -rf /tmp/clash.tar.gz >/dev/null 2>&1
-echo "Add OpenClash dev core ended"
-##---------------------------------------------------------
 
 Firmware_Diy_Start() {
 	ECHO "[Firmware_Diy_Start] Starting ..."
